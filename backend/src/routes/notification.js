0d2ee32ba090b57b46c0347c46f285fd01df9e7a
14-himanshu <klawchop1@gmail.com>
2026-04-13 19:43:14 +0530

    Delete backend/src/routes/notification.js

--- backend/src/routes/notification.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authenticateToken from "../utils/authMiddleware.js";
-import { myNotifications, readNotification } from "../controllers/notification.js";
-
-const router = express.Router();
-
-
-export default router;
