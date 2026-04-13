e8d4c77a0d99c2ae1202698f21d9a26a0aac9e00
14-himanshu <klawchop1@gmail.com>
2026-04-13 19:43:57 +0530

    Delete backend/src/routes/publicUser.js

--- backend/src/routes/publicUser.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getPublicUser } from "../controllers/publicUser.js";
-
-const router = express.Router();
-
-router.get("/:id", getPublicUser);
-
-export default router;
