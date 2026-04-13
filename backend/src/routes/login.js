945792640c759075ecd04add4f5f0db812759ca8
14-himanshu <klawchop1@gmail.com>
2026-04-13 19:42:55 +0530

    Delete backend/src/routes/login.js

--- backend/src/routes/login.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import cors from "cors"
-import login from "../controllers/login.js";
-import refreshToken from "../controllers/refreshToken.js";
-const router = express.Router();
-router.use(cors())
-router.post("/login",login);
-router.post("/refresh-token",refreshToken);
-export default router;
