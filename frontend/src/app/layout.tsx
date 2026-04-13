95f62d1c668bff1c44b5113e2814358ffd751049
Bineet Keshari <bineetkeshari@Bineets-MacBook-Pro.local>
2026-04-13 19:28:43 +0530

    edited frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ClientLayout from "../app/clientLayout.jsx";
+import ClientLayout from "./clientLayout.jsx";
 import Script from "next/script";   // ⬅ ADD THIS
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
